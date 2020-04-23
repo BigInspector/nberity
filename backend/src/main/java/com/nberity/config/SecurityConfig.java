4027e93335264e4a7a21c8b1ace9386a9b49db65
Karl Jürgenson <karl5@windowslive.com>
2020-04-23 20:49:23 +0300

    Attach security services to Angular code

--- backend/src/main/java/com/nberity/config/SecurityConfig.java
+++ backend/src/main/java/com/nberity/config/SecurityConfig.java
@@ -56,9 +56,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
+                .authorizeRequests().antMatchers("/authenticate", "/register")
+                .permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .permitAll()
                 // all other requests need to be authenticated
-                        anyRequest().authenticated().and().
+                .anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
                         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
--- frontend/src/app/authentication/authentication.service.ts
+++ frontend/src/app/authentication/authentication.service.ts
@@ -9,6 +9,13 @@ export class Customer{
 
 }
 
+export class JwtResponse{
+  constructor(
+    public jwttoken:string,
+  ) {}
+
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,15 +24,12 @@ export class AuthenticationService {
   constructor(private httpClient:HttpClient) { }
 
   authenticate(username, password) {
-    console.log(username);
-    console.log(password);
-    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
-    return this.httpClient.get<Customer>('http://localhost:8080/employees/validateLogin',{headers}).pipe(
+    return this.httpClient.post<any>('http://localhost:8080/authenticate',{username,password}).pipe(
       map(
         userData => {
           sessionStorage.setItem('username',username);
-          let authString = 'Basic ' + btoa(username + ':' + password);
-          sessionStorage.setItem('basicauth', authString);
+          let tokenStr= 'Bearer '+userData.token;
+          sessionStorage.setItem('token', tokenStr);
           return userData;
         }
       )
